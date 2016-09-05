2ab076ebae2fcfb6f7014df67ce887237ce9377b
okmanek <okmanek@onet.eu>
2016-09-05 07:17:44 +0200

    opens random link in a browser

--- randomArticle.py
+++ randomArticle.py
@@ -12,16 +12,14 @@ d = pq(url='https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles')
 
 #print(d.text())
 
-#print d('.wikitable td b a')
-
 #gets all links to articles, like:
 #'/wiki/Michael_Malloy'
 for link in d('.wikitable td b a'):
 	links.append(link.attrib['href'])
-	#print link.attrib['href']
-
+	
 articleNumber = randint(0, len(links))
 
-txt = 'https://en.wikipedia.org' + links[articleNumber]
+pageURL = 'https://en.wikipedia.org' + links[articleNumber]
+print 'URL to open: ' + pageURL
 
-print txt
+webbrowser.open(pageURL)
